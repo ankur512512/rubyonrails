339fa6a87103e4def4eb628c5ac67b1e97317920
Ankur Garg <ankur.garg@nuance.com>
2021-10-13 16:40:15 +0530

    adding sleep for db to get ready

--- development/entrypoint.sh
+++ development/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Sleeping for 5 seconds so that DB is ready to accept connections.
+sleep 5
+
+# Creating schema
 rails db:setup
 
+# Starting rails server on HTTPS
 rails s -b 'ssl://0.0.0.0:3000?key=config/certs/dev.key&cert=config/certs/dev.crt'
--- production/entrypoint.sh
+++ production/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Sleeping for 5 seconds so that DB is ready to accept connections.
+sleep 5
+
 # Doing db related setup
 rails db:setup
 
-# Running rails server
+# Running rails server on HTTPS
 rails s -b 'ssl://0.0.0.0:3000?key=config/certs/prod.key&cert=config/certs/prod.crt'
